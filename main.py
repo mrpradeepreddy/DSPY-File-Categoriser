d01d4e5dd139193eef7346cc84e5de328c6abc8b
Pradeep Reddy <mrppradeepreddy@gmail.com>
2025-10-01 13:18:52 +0530

    made changes in prompts for the

--- main.py
+++ main.py
@@ -29,18 +29,18 @@ class ExtractMedicalMetadata(dspy.Signature):
     """Extract structured metadata fields from a medical document."""
     document_text = dspy.InputField(desc="The content of the medical PDF.")
 
-    Classification = dspy.OutputField(desc="The classification of the document like Principal Investigator Curriculum Vitae ,Sub-Investigator Curriculum Vitae ,Site Training Material ,Site Evidence of Training ,N/A")
-    DocumentDate = dspy.OutputField(desc="The date the document was created or signed like mm-dd-yyyy") 
-    LabName = dspy.OutputField(desc="Extract the name of the laboratory associated with the site as specified in the document. Note that the extracted lab name is distinct from the site name. Display the extracted lab name as Lab Name in the output. If no lab name is mentioned or identifiable, leave the field blank.")
-    ExpirationDate = dspy.OutputField(desc="The expiration date, if available like mm-dd-yyyy")
-    ClassificationReason = dspy.OutputField(desc="The reason for classification")
-    PersonnelName = dspy.OutputField(desc="The name of the personnel mentioned")
-    OrganizationName = dspy.OutputField(desc="The name of the organization involved")
+    Classification = dspy.OutputField(desc="The classification of the document like Principal Investigator Curriculum Vitae ,Sub-Investigator Curriculum Vitae ,Site Training Material ,Site Evidence of Training ,if the value doesnot exist then return N/A")
+    DocumentDate = dspy.OutputField(desc="The date the document was created or signed like mm-dd-yyyy. if the value doesnot exist then return N/A") 
+    LabName = dspy.OutputField(desc="Extract the name of the laboratory associated with the site as specified in the documment like Site Name: Kevinstad Memorial Clinic.Display the extracted lab name as Lab Name in the output. If no lab name is mentioned or identifiable, leave the field blank. ")
+    ExpirationDate = dspy.OutputField(desc="The expiration date, if available like mm-dd-yyyy. If no expiration date is mentioned or identifiable, leave the field blank or return N/A.")
+    ClassificationReason = dspy.OutputField(desc="The reason for classification like Syneos Health")
+    PersonalName = dspy.OutputField(desc="Extract the name of the person mentioned in the text.That may be indicated as the Personal name=Dr. John Doe or Investigator Name: John Doe. If no personal name is mentioned or identifiable, leave the field blank.")
+    OrganizationName = dspy.OutputField(desc="The name of the organization involved .If no organization name is mentioned or identifiable, leave the field blank or return N/A.")
     StudyName = dspy.OutputField(desc="The study name related to the document")
-    Country = dspy.OutputField(desc="The country where the study or lab is located like United States,Canada,India,N/A")
-    SiteNumber = dspy.OutputField(desc="The site number, if available")
-    Type=dspy.OutputField(desc="The type of document like Site Management,N/A")
-    Subtype=dspy.OutputField(desc="The subtype of document like Site Set-up,Site Initiation,General,Site Selection,Site Management,N/A")
+    Country = dspy.OutputField(desc="The country where the study or lab is located like United States,Canada,India or return N/A if not available")
+    SiteNumber = dspy.OutputField(desc="The site number, if available like  1007 or  Site Number: 1007")
+    Type=dspy.OutputField(desc="The type of document like Site Management,if there is no type then return N/A")
+    Subtype=dspy.OutputField(desc="The subtype of document like Site Set-up,Site Initiation,General,Site Selection,Site Management.If there is no subtype then return N/A")
 
 class MedicalQASystem(dspy.Module):
     def __init__(self):
@@ -55,7 +55,7 @@ class MedicalQASystem(dspy.Module):
             LabName=metadata.LabName,
             ExpirationDate=metadata.ExpirationDate,
             ClassificationReason=metadata.ClassificationReason,
-            PersonnelName=metadata.PersonnelName,
+            PersonalName=metadata.PersonalName,
             OrganizationName=metadata.OrganizationName,
             StudyName=metadata.StudyName,
             Country=metadata.Country,
@@ -71,7 +71,7 @@ def simple_accuracy(gold, pred,trace=None):
     correct = sum(getattr(gold, f) == getattr(pred, f)
         for f in [
             "Classification","DocumentDate","LabName","ExpirationDate",
-            "ClassificationReason","PersonnelName","OrganizationName",
+            "ClassificationReason","PersonalName","OrganizationName",
             "StudyName","Country","SiteNumber","Type","Subtype"
         ]
     )
@@ -103,7 +103,7 @@ if uploaded_file is not None:
             "LabName": prediction.LabName,
             "ExpirationDate": prediction.ExpirationDate,
             "ClassificationReason": prediction.ClassificationReason,
-            "PersonnelName": prediction.PersonnelName,
+            "PersonalName": prediction.PersonalName,
             "OrganizationName": prediction.OrganizationName,
             "StudyName": prediction.StudyName,
             "Country": prediction.Country,
